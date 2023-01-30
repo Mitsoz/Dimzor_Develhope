d8a95d901c44adb2a0f4323be36477b2c801fb1d
Dimitris <115027454+Mitsoz@users.noreply.github.com>
2023-01-30 20:07:35 +0200

    Update Dag_Airflow_Exercise_1.py

--- Dag_Airflow_Exercise_1.py
+++ Dag_Airflow_Exercise_1.py
@@ -7,8 +7,9 @@ default_dag_args = { 'start_date': datetime(2022, 1, 1),
  'email_on_failure': False, 'email_on_retry': False,
   'retries': 1, 'retry_delay': timedelta(minutes=5), 'project_id': 1 }
 
-task_0 = BashOperator(task_id = 'bash_task', bash_command = "echo 'command executed from Bash Operator' ")
-task_1 = BashOperator(task_id = 'bash_task_move_data', bash_command = "cp /Users/Username/develhope_file/DATA_CENTER/DATA_LAKE/dataset_raw.txt  /Users/Username/develhope_file/DATA_CENTER/CLEAN_DATA")
-task_2 = BashOperator(task_id = 'bash_task_move_data', bash_command = "os.remove(/Users/Username/develhope_file/DATA_CENTER/DATA_LAKE/dataset_raw.txt)")
+with DAG("First_DAG", schedule_interval = None, default_args = default_dag_args) as dag:    
+  task_0 = BashOperator(task_id = 'bash_task', bash_command = "echo 'command executed from Bash Operator' ")
+  task_1 = BashOperator(task_id = 'bash_task_move_data', bash_command = "cp /Users/Username/develhope_file/DATA_CENTER/DATA_LAKE/dataset_raw.txt  /Users/Username/develhope_file/DATA_CENTER/CLEAN_DATA")
+  task_2 = BashOperator(task_id = 'bash_task_move_data', bash_command = "os.remove(/Users/Username/develhope_file/DATA_CENTER/DATA_LAKE/dataset_raw.txt)")
 
 task_0 >> task_1 >> task_2
